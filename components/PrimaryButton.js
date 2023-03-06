76894b99600fb7f971453863c5bdab2bd4b19bcf
Roman Tyniv <roman.tyniv@3ss.tv>
2023-03-06 22:31:29 +0200

    first changes

---
+++ components/PrimaryButton.js
@@ -0,0 +1,12 @@
+
+import { View, Text } from 'react-native';
+
+function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
+
+export default PrimaryButton;
